462ab6ee21b22b299e69594f7bac4e3387d78776
lucky-elsa <beautylotus718@gmail.com>
2024-06-30 21:50:40 -0700

    fix: integrated track api

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,9 +1,11 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Navbar } from "@/components/Navbar";
 import { TbDotsVertical } from "react-icons/tb";
 import CardWrapper from "@/components/CardWrapper";
 import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import useUserStore from "@/store/useStore";
+import api from "@/lib/api";
 
 const SignalInfoCard = ({
   title,
@@ -29,6 +31,27 @@ const SignalInfoCard = ({
 };
 
 export default function FriendsPage() {
+  const [trackData, setTrackData] = useState<any>([]);
+
+  const { userId, username } = useUserStore((state: any) => ({
+    userId: state.userId,
+    username: state.username,
+  }));
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/api/creators/");
+
+        setTrackData(response.data?.result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex w-full min-h-screen justify-center bg-background p-3 pb-[100px] ">
       <div className="relative w-full flex flex-col items-center justify-start animate-opacity-scale">
@@ -46,28 +69,21 @@ export default function FriendsPage() {
           <div className="w-full mt-6 mb-7 h-[1px] bg-[#FFFFFF1A]"></div>
         </div>
         <div className="w-full flex flex-col gap-[10px]">
-          <CardWrapper className="h-[54px]">
-            <SignalInfoCard
-              title="@CryptoCuru"
-              subTitle="Detailed insights on market trends and forecasts"
-            >
-              <Avatar>
-                <AvatarImage src={"/images/signal-avatar-1.png"} />
-                <AvatarFallback>Host</AvatarFallback>
-              </Avatar>
-            </SignalInfoCard>
-          </CardWrapper>
-          <CardWrapper className="h-[54px]">
-            <SignalInfoCard
-              title="@TokenExpert"
-              subTitle="Analysis of emerging tokens and market movements"
-            >
-              <Avatar>
-                <AvatarImage src={"/images/signal-avatar-2.png"} />
-                <AvatarFallback>Host</AvatarFallback>
-              </Avatar>
-            </SignalInfoCard>
-          </CardWrapper>
+          {trackData.map((track: any, index: number) => {
+            return (
+              <CardWrapper key={index} className="h-[54px]">
+                <SignalInfoCard
+                  title={"@" + track?.twitter_username}
+                  subTitle={track?.twitter_description}
+                >
+                  <Avatar>
+                    <AvatarImage src={track?.twitter_profile_image_url} />
+                    <AvatarFallback>Host</AvatarFallback>
+                  </Avatar>
+                </SignalInfoCard>
+              </CardWrapper>
+            );
+          })}
           <CardWrapper className="h-[54px] bg-[#6174EC33]">
             <SignalInfoCard title="Add Host" icon>
               <Avatar>
